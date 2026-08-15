a2d87a253a7d3d175381592a833648ce6e6feef9
uzairburki-div <uzairburki41@gmail.com>
2026-08-15 14:31:05 +0500

    Debuging

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 // Serve portfolio files
 app.use(express.static(__dirname));
 
